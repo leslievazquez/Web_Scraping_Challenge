b170e8f1bb4b166f14c9fcad6dc9d139622a71f0
Leslie Vazquez <69220324+leslievazquez@users.noreply.github.com>
2020-12-12 09:56:01 -0500

    Update app.py

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,41 +1,44 @@
-from webdriver_manager.chrome import ChromeDriverManager
-from flask import Flask, render_template, redirect
+# Import Dependencies 
+from flask import Flask, render_template, redirect 
 from flask_pymongo import PyMongo
 import scrape_mars
+import os
 
 
-# Create an instance of Flask
+# Hidden authetication file
+#import config 
+
+# Create an instance of Flask app
 app = Flask(__name__)
 
-# Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mars_app")
 
-# Route to render index.html template using data from Mongo
+# Use flask_pymongo to set up mongo connection locally 
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# Create route that renders index.html template and finds documents from mongo
 @app.route("/")
-def home():
+def home(): 
 
-    # Find one record of data from the mongo database
-    mars_data_dict = mongo.db.mars_data_dict.find_one()
+    # Find data
+    mars_info = mongo.db.mars_info.find_one()
 
     # Return template and data
-    return render_template("index.html", mars_data_dict = mars_data_dict)
+    return render_template("index.html", mars_info=mars_info)
 
-
-# Route that will trigger the scrape function
+# Route that will trigger scrape function
 @app.route("/scrape")
-def scrape():
+def scrape(): 
 
-    mars_data_dict = mongo.db.mars_data_dict
+    # Run scrapped functions
+    mars_info = mongo.db.mars_info
+    mars_data = scrape_mars.scrape_mars_news()
+    mars_data = scrape_mars.scrape_mars_image()
+    mars_f = scrape_mars.scrape_mars_facts()
+    mars_data = scrape_mars.scrape_mars_hemispheres()
+    mars_info.update({}, mars_data, upsert=True)
 
-    # Run the scrape function
-    mars_data = scrape_mars.scrape()
-
-    # Update the Mongo database using update and upsert=True
-    mars_data_dict.update({}, mars_data, upsert=True)
-
-    # Redirect back to home page
     return redirect("/", code=302)
 
-
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == "__main__": 
+    app.run(debug= True)
